6add1ded7b51be06dc4f5bf5510321a3675503f4
NickhilPant <nickpant6@gmail.com>
2022-11-06 22:33:45 -0600

    Made it look better :)

--- client/src/components/course.jsx
+++ client/src/components/course.jsx
@@ -1,7 +1,7 @@
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 function Course(props){
-    return <Card style={{ width: '18rem' }}>
+    return <Card style={{ width: '30rem' }}>
     <Card.Header>{props.info.courseName}</Card.Header>
     <ListGroup variant="flush">
       <ListGroup.Item>{props.info.coursePrefix + " " + props.info.courseNumber}</ListGroup.Item>
--- client/src/components/transcript.jsx
+++ client/src/components/transcript.jsx
@@ -1,20 +1,21 @@
 import Course from "./course"
 import Term from "./term"
 import Badge from 'react-bootstrap/Badge';
+import './transcript.css'
 
 
 function Transcript(props){
 
     const array = props.transcript.student.terms.map((value, idx)=>{
         const c = value.courses.map((v, idx)=>{
-            return <Course info = {v}/>
+            return <div className="bdn"><Course info = {v}/></div>
         })
         return <div key={idx}>
             <Term name={value.name} year={value.year}/>
             {c}
             </div> 
     })
-    return <div>
+    return <div className="transcript">
         <h1>
         <Badge bg="secondary">Student: {props.transcript.student.name}</Badge>
         <br />
